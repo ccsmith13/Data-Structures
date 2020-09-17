bf35ede65fc984eff9a5a3012a3f8e3915af4f21
Christine Smith <christinecsmith1@gmail.com>
2020-09-16 20:39:54 -0700

    finished module 1 + stretch and started module 2 for the doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,7 +7,14 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
-            
+
+    def get_value(self):
+        return self.value
+    def get_next_node(self):
+        return self.next
+    def set_next_node(self, new_next):
+        self.next_node = new_next
+                
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -19,7 +26,24 @@ class DoublyLinkedList:
         self.length = 1 if node is not None else 0
 
     def __len__(self):
+        temp = self.head
+        count = 0
+        while(temp):
+            count += 1
+            temp = temp.get_next_node()
+        self.length = count
         return self.length
+
+    def get_prev_node(self, node):
+        if self.head == None or self.head == self.tail:
+            return None
+        else: 
+            cur_node = self.head
+            while cur_node is not None:
+                if cur_node.get_next_node() == node:
+                    return cur_node
+                else:
+                    cur_node = cur_node.get_next_node()
     
     """
     Wraps the given value in a ListNode and inserts it 
@@ -27,15 +51,35 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
-        
+        if self.head == None: 
+            new_node = ListNode(value)
+            self.head = new_node
+            self.tail = new_node
+        elif self.head == self.tail: 
+            new_node = ListNode(value, None, self.head)
+            self.head = new_node
+            self.tail = self.head 
+        else: 
+            new_node = ListNode(value, None, self.head)
+            self.head = new_node
+
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        if self.head == None: 
+            return None
+        else:
+            ret_value = self.head.get_value()
+            if self.head == self.tail: 
+                self.head = None
+                self.tail = None
+                return ret_value
+            else:
+                self.head = self.head.get_next_node()
+                return ret_value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +87,17 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        if self.head == None: 
+            new_node = ListNode(value)
+            self.head = new_node
+            self.tail = new_node
+        elif self.head == self.tail: 
+            new_node = ListNode(value, self.tail, None)
+            self.tail = new_node
+            self.head = self.tail 
+        else:
+            new_node = ListNode(value, self.tail, None)
+            self.tail = new_node
             
     """
     Removes the List's current tail node, making the 
@@ -51,7 +105,17 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        if self.tail == None: 
+            return None
+        else:
+            ret_value = self.tail.get_value()
+            if self.head == self.tail: 
+                self.head = None
+                self.tail = None
+                return ret_value
+            else:
+                self.tail = self.get_prev_node(self.tail)
+                return ret_value
             
     """
     Removes the input node from its current spot in the 
@@ -79,4 +143,15 @@ class DoublyLinkedList:
     in the List.
     """
     def get_max(self):
-        pass
+        if self.head == None: 
+            return None
+        elif self.head == self.tail: 
+            return self.head.get_value()
+        else:
+            cur_node = self.head
+            next_node = cur_node.get_next_node()
+            while cur_node is not None: 
+                if cur_node.get_value() < next_node.get_value():
+                    cur_node = next_node 
+                else: 
+                    return cur_node.get_value()
